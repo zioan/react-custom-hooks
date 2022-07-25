3ba829cab90b5925e3cbacd78c05f2129f26c6f0
Ioan Zaharia <zaharia.ioan@gmail.com>
2022-07-25 11:24:00 +0200

    fetch hook

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+import { useEffect } from 'react';
+import useFetch from './hooks/useFetch';
 import './App.css';
 
 function App() {
+  const { data, isPending, error } = useFetch('https://reqres.in/api/users');
+
+  useEffect(() => {
+    console.log(data);
+  }, [data]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div id='useFetchHook'>
+        {error && <p>{error}</p>}
+        {isPending && <p>Loading...</p>}
+      </div>
     </div>
   );
 }
